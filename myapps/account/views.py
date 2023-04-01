60536c29c4513288e04c451df970e766346a393a
nhchung <nhchung.web@gmail.com>
2023-04-02 00:07:22 +0700

    add code

---
+++ myapps/account/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    pass
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
+def register(request):
+    pass
+
+def forgot_password(request):
+    pass
+
+def change_password(request):
+    pass
+
+def login_google(request):
+    pass
+
+def login_facebook(request):
+    pass
